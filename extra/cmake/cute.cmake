5341313c0f6c78d8d9b14681f571829559a4acb7
mathewmariani <someone.mariani@gmail.com>
2025-09-23 23:57:51 -0400

    cmake fix

--- extra/cmake/cute.cmake
+++ extra/cmake/cute.cmake
@@ -4,7 +4,7 @@ option(BUILD_WITH_CUTE "cute Enabled" ${BUILD_WITH_CUTE_DEFAULT})
 message("BUILD_WITH_CUTE: ${BUILD_WITH_CUTE}")
 
 if(BUILD_WITH_CUTE)
-  set(CUTE_DIR "${THIRDPARTY_DIR}/cute")
+  set(CUTE_DIR ${THIRDPARTY_DIR}/cute)
   set(CUTE_SRC ${CUTE_DIR}/cute.c)
 
   add_library(cute STATIC ${CUTE_SRC})
