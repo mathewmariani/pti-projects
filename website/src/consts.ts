f0235a4b79ccd246b1c1cb6279784a7d71739e2e
mathewmariani <someone.mariani@gmail.com>
2025-09-23 23:07:38 -0400

    Environment aware variables

--- website/src/consts.ts
+++ website/src/consts.ts
@@ -1,9 +1,10 @@
 export const SITE_TITLE = 'pti-projects';
 export const SITE_DESCRIPTION = '';
 
-export const SYMLINK_SAMPLES_SRC = 'build/web-debug/projects/samples';
-export const SYMLINK_SAMPLES_DST = 'website/public/projects/samples';
-export const SYMLINK_TESTS_SRC = 'build/web-debug/projects/platformer';
-export const SYMLINK_TESTS_DST = 'website/public/projects/platformer';
+const folder = import.meta.env.PROD ? 'web-release' : 'web-debug';
+export const SYMLINK_SAMPLES_SRC = `build/${folder}/projects/samples`;
+export const SYMLINK_SAMPLES_DST = `website/public/projects/samples`;
+export const SYMLINK_TESTS_SRC = `build/${folder}/projects/platformer`;
+export const SYMLINK_TESTS_DST = `website/public/projects/platformer`;
 
 export const HomeLink = (): string => '/pti-projects/';
