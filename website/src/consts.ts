6ad69bb0375f0dab5298946ec9d705b1740a9ab9
mathewmariani <someone.mariani@gmail.com>
2025-10-31 18:25:25 -0400

    Added coinrush to website

--- website/src/consts.ts
+++ website/src/consts.ts
@@ -7,6 +7,7 @@ export const SYMLINKS = [
   { source: `build/${folder}/projects/platformer`, target: `website/public/projects/platformer` },
   { source: `build/${folder}/projects/towerfall-physics`, target: `website/public/projects/towerfall-physics` },
   { source: `build/${folder}/projects/zombies`, target: `website/public/projects/zombies` },
+  { source: `build/${folder}/projects/coinrush`, target: `website/public/projects/coinrush` },
 ] as const;
 
 export const HomeLink = (): string => '/pti-projects/';
