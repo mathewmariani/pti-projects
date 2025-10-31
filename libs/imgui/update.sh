f43c624fbdfabd2430544a1c0d2ca2202002ac13
mathewmariani <someone.mariani@gmail.com>
2025-10-31 18:31:59 -0400

    Updated imgui

--- libs/imgui/update.sh
+++ libs/imgui/update.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# update the required sokol headers
 
 # repo url
 IMGUI_PREFIX="https://raw.githubusercontent.com/ocornut/imgui/master"
 
 # headers
 IMGUI_CONFIG_H="$IMGUI_PREFIX/imconfig.h"
-IMGUI_DEMO_CPP="$IMGUI_PREFIX/imgui_demo.cpp"
 IMGUI_DRAW_CPP="$IMGUI_PREFIX/imgui_draw.cpp"
 IMGUI_INTERNAL_H="$IMGUI_PREFIX/imgui_internal.h"
 IMGUI_TABLES_CPP="$IMGUI_PREFIX/imgui_tables.cpp"
@@ -17,6 +15,11 @@ IMGUI_STB_RECTPACK_H="$IMGUI_PREFIX/imstb_rectpack.h"
 IMGUI_STB_TEXTEDIT_H="$IMGUI_PREFIX/imstb_textedit.h"
 IMGUI_STB_TRUETYPE_H="$IMGUI_PREFIX/imstb_truetype.h"
 
+# backends
+IMGUI_BACKEND_OPENGL3_CPP="$IMGUI_PREFIX/backends/imgui_impl_opengl3.cpp"
+IMGUI_BACKEND_OPENGL3_H="$IMGUI_PREFIX/backends/imgui_impl_opengl3.h"
+IMGUI_BACKEND_OPENGL3_LOADER_H="$IMGUI_PREFIX/backends/imgui_impl_opengl3_loader.h"
+
 # license
 IMGUI_LICENSE="$IMGUI_PREFIX/LICENSE.txt"
 
@@ -25,7 +28,6 @@ OUTPUT=./libs/imgui
 
 # curl all files
 curl $IMGUI_CONFIG_H > $OUTPUT/imconfig.h
-curl $IMGUI_DEMO_CPP > $OUTPUT/imgui_demo.cpp
 curl $IMGUI_DRAW_CPP > $OUTPUT/imgui_draw.cpp
 curl $IMGUI_INTERNAL_H > $OUTPUT/imgui_internal.h
 curl $IMGUI_TABLES_CPP > $OUTPUT/imgui_tables.cpp
@@ -35,6 +37,11 @@ curl $IMGUI_IMGUI_H > $OUTPUT/imgui.h
 curl $IMGUI_STB_RECTPACK_H > $OUTPUT/imstb_rectpack.h
 curl $IMGUI_STB_TEXTEDIT_H > $OUTPUT/imstb_textedit.h
 curl $IMGUI_STB_TRUETYPE_H > $OUTPUT/imstb_truetype.h
+
+curl $IMGUI_BACKEND_OPENGL3_CPP > $OUTPUT/imgui_impl_opengl3.cpp
+curl $IMGUI_BACKEND_OPENGL3_H > $OUTPUT/imgui_impl_opengl3.h
+curl $IMGUI_BACKEND_OPENGL3_LOADER_H > $OUTPUT/imgui_impl_opengl3_loader.h
+
 
 # license
 curl $IMGUI_LICENSE > $OUTPUT/LICENSE.txt
