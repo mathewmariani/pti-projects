2c3c76490887d5e3676ffec1c0dbcf1d87989cb2
mathewmariani <someone.mariani@gmail.com>
2025-11-08 23:28:13 +0000

    Deploying to gh-pages from @ mathewmariani/pti-projects@a21218efbe1ca97bac51ea4cadfe77b03e2bfe38 🚀

---
+++ projects/coinrush/assets.js
@@ -0,0 +1,134 @@
+
+  var Module = typeof Module != 'undefined' ? Module : {};
+
+  if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
+  Module['expectedDataFileDownloads']++;
+  (() => {
+    // Do not attempt to redownload the virtual filesystem data when in a pthread or a Wasm Worker context.
+    var isPthread = typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD;
+    var isWasmWorker = typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER;
+    if (isPthread || isWasmWorker) return;
+    var isNode = globalThis.process && globalThis.process.versions && globalThis.process.versions.node && globalThis.process.type != 'renderer';
+    async function loadPackage(metadata) {
+
+      var PACKAGE_PATH = '';
+      if (typeof window === 'object') {
+        PACKAGE_PATH = window['encodeURIComponent'](window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')) + '/');
+      } else if (typeof process === 'undefined' && typeof location !== 'undefined') {
+        // web worker
+        PACKAGE_PATH = encodeURIComponent(location.pathname.substring(0, location.pathname.lastIndexOf('/')) + '/');
+      }
+      var PACKAGE_NAME = '/home/runner/work/pti-projects/pti-projects/build/web-release/projects/coinrush/Release/assets.data';
+      var REMOTE_PACKAGE_BASE = 'assets.data';
+      var REMOTE_PACKAGE_NAME = Module['locateFile'] ? Module['locateFile'](REMOTE_PACKAGE_BASE, '') : REMOTE_PACKAGE_BASE;
+      var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
+
+      async function fetchRemotePackage(packageName, packageSize) {
+        if (isNode) {
+          var contents = require('fs').readFileSync(packageName);
+          return new Uint8Array(contents).buffer;
+        }
+        if (!Module['dataFileDownloads']) Module['dataFileDownloads'] = {};
+        try {
+          var response = await fetch(packageName);
+        } catch (e) {
+          throw new Error(`Network Error: ${packageName}`, {e});
+        }
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.url}`);
+        }
+
+        const chunks = [];
+        const headers = response.headers;
+        const total = Number(headers.get('Content-Length') || packageSize);
+        let loaded = 0;
+
+        Module['setStatus'] && Module['setStatus']('Downloading data...');
+        const reader = response.body.getReader();
+
+        while (1) {
+          var {done, value} = await reader.read();
+          if (done) break;
+          chunks.push(value);
+          loaded += value.length;
+          Module['dataFileDownloads'][packageName] = {loaded, total};
+
+          let totalLoaded = 0;
+          let totalSize = 0;
+
+          for (const download of Object.values(Module['dataFileDownloads'])) {
+            totalLoaded += download.loaded;
+            totalSize += download.total;
+          }
+
+          Module['setStatus'] && Module['setStatus'](`Downloading data... (${totalLoaded}/${totalSize})`);
+        }
+
+        const packageData = new Uint8Array(chunks.map((c) => c.length).reduce((a, b) => a + b, 0));
+        let offset = 0;
+        for (const chunk of chunks) {
+          packageData.set(chunk, offset);
+          offset += chunk.length;
+        }
+        return packageData.buffer;
+      }
+
+      var fetchPromise;
+      var fetched = Module['getPreloadedPackage']?.(REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE);
+
+      if (!fetched) {
+        // Note that we don't use await here because we want to execute the
+        // the rest of this function immediately.
+        fetchPromise = fetchRemotePackage(REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE);
+      }
+
+    async function runWithFS(Module) {
+
+      function assert(check, msg) {
+        if (!check) throw new Error(msg);
+      }
+Module['FS_createPath']("/", "assets", true, true);
+Module['FS_createPath']("/assets", "levels", true, true);
+
+      for (var file of metadata['files']) {
+        var name = file['filename']
+        Module['addRunDependency'](`fp ${name}`);
+      }
+
+      async function processPackageData(arrayBuffer) {
+        assert(arrayBuffer, 'Loading data file failed.');
+        assert(arrayBuffer.constructor.name === ArrayBuffer.name, 'bad input to processPackageData ' + arrayBuffer.constructor.name);
+        var byteArray = new Uint8Array(arrayBuffer);
+        var curr;
+        // Reuse the bytearray from the XHR as the source for file reads.
+          for (var file of metadata['files']) {
+            var name = file['filename'];
+            var data = byteArray.subarray(file['start'], file['end']);
+            // canOwn this data in the filesystem, it is a slice into the heap that will never change
+          Module['FS_createDataFile'](name, null, data, true, true, true);
+          Module['removeRunDependency'](`fp ${name}`);
+          }
+          Module['removeRunDependency']('datafile_/home/runner/work/pti-projects/pti-projects/build/web-release/projects/coinrush/Release/assets.data');
+      }
+      Module['addRunDependency']('datafile_/home/runner/work/pti-projects/pti-projects/build/web-release/projects/coinrush/Release/assets.data');
+
+      if (!Module['preloadResults']) Module['preloadResults'] = {};
+
+      Module['preloadResults'][PACKAGE_NAME] = {fromCache: false};
+      if (!fetched) {
+        fetched = await fetchPromise;
+      }
+      processPackageData(fetched);
+
+    }
+    if (Module['calledRun']) {
+      runWithFS(Module);
+    } else {
+      if (!Module['preRun']) Module['preRun'] = [];
+      Module['preRun'].push(runWithFS); // FS is not initialized yet, wait for it
+    }
+
+    }
+    loadPackage({"files": [{"filename": "/assets/bullet.ase", "start": 0, "end": 420}, {"filename": "/assets/cannon.ase", "start": 420, "end": 1041}, {"filename": "/assets/coin.ase", "start": 1041, "end": 1739}, {"filename": "/assets/collect.ase", "start": 1739, "end": 2592}, {"filename": "/assets/collect2.ase", "start": 2592, "end": 3072}, {"filename": "/assets/dog.ase", "start": 3072, "end": 3658}, {"filename": "/assets/door.ase", "start": 3658, "end": 4241}, {"filename": "/assets/font.ase", "start": 4241, "end": 5807}, {"filename": "/assets/heart.ase", "start": 5807, "end": 6185}, {"filename": "/assets/levels/01.ase", "start": 6185, "end": 8404}, {"filename": "/assets/levels/02.ase", "start": 8404, "end": 10666}, {"filename": "/assets/levels/03.ase", "start": 10666, "end": 12866}, {"filename": "/assets/levels/04.ase", "start": 12866, "end": 15072}, {"filename": "/assets/levels/05.ase", "start": 15072, "end": 17293}, {"filename": "/assets/levels/06.ase", "start": 17293, "end": 19508}, {"filename": "/assets/platform.ase", "start": 19508, "end": 19895}, {"filename": "/assets/tilemap.ase", "start": 19895, "end": 22113}, {"filename": "/assets/zombie.ase", "start": 22113, "end": 22705}], "remote_package_size": 22705});
+
+  })();
