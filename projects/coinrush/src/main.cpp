f232219c4a52bca5255baa2bb9021c4283221473
mathewmariani <someone.mariani@gmail.com>
2025-11-12 19:23:50 -0500

    Initial debug for coin rush

--- projects/coinrush/src/main.cpp
+++ projects/coinrush/src/main.cpp
@@ -10,6 +10,11 @@
 
 #include <string>
 
+#if defined(PTI_DEBUG)
+#include "imgui/imgui.h"
+bool show_overlays = false;
+#endif
+
 static void load(void) {
 	GameStateInit();
 	batteries::reload();
@@ -95,14 +100,70 @@ static void frame(void) {
 	std::string death_str(buffer);
 	pti_print(death_str.c_str(), 8, 16);
 
-	// debugging:
-	// for (auto *e : GetEntitiesOfType<Actor>()) {
-	// 	pti_rect(e->position.x + e->bx, e->position.y + e->by, e->bw - 1, e->bh - 1, 0xff00ff00);
-	// }
-	// for (auto *e : GetEntitiesOfType<Solid>()) {
-	// 	pti_rect(e->position.x + e->bx, e->position.y + e->by, e->bw - 1, e->bh - 1, 0xff00ff00);
-	// }
+#if defined(PTI_DEBUG)
+	if (show_overlays) {
+		for (auto *e : GetEntitiesOfType<EntityBase>()) {
+			pti_rect(e->position.x + e->bx, e->position.y + e->by, e->bw - 1, e->bh - 1, 0xff00ff00);
+		}
+	}
+#endif
 }
+
+#if defined(PTI_DEBUG)
+void debug(void) {
+	ImGui::Begin("CoinRush", NULL, ImGuiWindowFlags_AlwaysAutoResize);
+
+	const auto &gameState = GetGameState();
+
+	if (ImGui::CollapsingHeader("core", ImGuiTreeNodeFlags_DefaultOpen)) {
+		ImGui::Text("fps: %.1f", ImGui::GetIO().Framerate);
+
+		const size_t used_bytes = 0;    //(size_t) (_pti.cart.it - _pti.cart.begin);
+		const size_t capacity_bytes = 0;//(size_t) (_pti.cart.cap - _pti.cart.begin);
+		const float used_kb = used_bytes / 1024.0f;
+		const float capacity_kb = capacity_bytes / 1024.0f;
+
+		ImGui::Text("usage: %.2f KB / %.2f KB (%.2f%%)\n", used_kb, capacity_kb, (used_kb / capacity_kb) * 100.0);
+	}
+
+	if (ImGui::CollapsingHeader("game", ImGuiTreeNodeFlags_DefaultOpen)) {
+		int cam_x, cam_y;
+		pti_get_camera(&cam_x, &cam_y);
+		ImGui::Text("camera: (%d, %d)", cam_x, cam_y);
+
+		auto entities = GetEntitiesOfType<EntityBase>();
+		ImGui::Text("entities: (%d)", (int) entities.size());
+
+		ImGui::Checkbox("overlay", &show_overlays);
+
+		if (ImGui::Button("reload level")) {
+			std::printf("reload level\n");
+		}
+	}
+
+	if (ImGui::CollapsingHeader("entities", ImGuiTreeNodeFlags_DefaultOpen)) {
+		auto players = GetEntitiesOfType<Player>();
+
+		if (players.size() > 0) {
+			auto *player = players[0];
+			ImGui::Text("position: (%d, %d)", player->position.x, player->position.y);
+			ImGui::Text("speed: (%.2f, %.2f)", player->speed.x, player->speed.y);
+			ImGui::Text("grounded: (%s)", player->IsGrounded() ? "true" : "false");
+
+			if (ImGui::Button("hurt")) {
+				std::printf("hurt\n");
+			}
+			if (ImGui::Button("kill")) {
+				std::printf("kill\n");
+			}
+		} else {
+			ImGui::Text("player is dead\n");
+		}
+	}
+
+	ImGui::End();
+}
+#endif
 
 pti_desc pti_main(int argc, char *argv[]) {
 
@@ -110,6 +171,9 @@ pti_desc pti_main(int argc, char *argv[]) {
 			.init_cb = init,
 			.frame_cb = frame,
 			.cleanup_cb = cleanup,
+#if defined(PTI_DEBUG)
+			.debug_cb = debug,
+#endif
 			.memory_size = _pti_kilobytes(512), /* 256KB */
 			.width = kScreenWidth,
 			.height = kScreenHeight,
