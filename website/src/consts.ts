580adb130a1a467eb032ae186e74171ffa4b4500
mathewmariani <someone.mariani@gmail.com>
2026-01-03 14:46:42 -0500

    Added to website

--- website/src/consts.ts
+++ website/src/consts.ts
@@ -10,6 +10,7 @@ export const SYMLINKS = [
   { source: `build/${folder}/projects/coinrush`, target: `website/public/projects/coinrush` },
   { source: `build/${folder}/projects/rocktoss`, target: `website/public/projects/rocktoss` },
   { source: `build/${folder}/projects/menu`, target: `website/public/projects/menu` },
+  { source: `build/${folder}/projects/controller`, target: `website/public/projects/controller` },
 ] as const;
 
 export const HomeLink = (): string => '/pti-projects/';
