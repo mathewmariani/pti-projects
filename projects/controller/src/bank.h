adc025b3dd78df0df7b7f9730075e7712781d12c
mathewmariani <someone.mariani@gmail.com>
2026-01-03 14:31:40 -0500

    Added controller demo

---
+++ projects/controller/src/bank.h
@@ -0,0 +1,19 @@
+#pragma once
+
+typedef struct pti_bitmap_t pti_bitmap_t;
+
+// font
+extern pti_bitmap_t *bitmap_a;
+extern pti_bitmap_t *bitmap_b;
+extern pti_bitmap_t *bitmap_x;
+extern pti_bitmap_t *bitmap_y;
+
+extern pti_bitmap_t *bitmap_left;
+extern pti_bitmap_t *bitmap_right;
+extern pti_bitmap_t *bitmap_up;
+extern pti_bitmap_t *bitmap_down;
+
+extern pti_bitmap_t *bitmap_start;
+extern pti_bitmap_t *bitmap_select;
+extern pti_bitmap_t *bitmap_rshoulder;
+extern pti_bitmap_t *bitmap_lshoulder;
