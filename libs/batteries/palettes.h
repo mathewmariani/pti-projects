9d13ffa11be9c5e079fd3de1709c004722ca1567
mathewmariani <someone.mariani@gmail.com>
2026-01-05 14:15:21 -0500

    Added palette to batteries

---
+++ libs/batteries/palettes.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+// sweetie16 palette
+// ABGR
+static uint32_t sweetie16[] = {
+		0xff2c1c1a,
+		0xff5d275d,
+		0xff533eb1,
+		0xff577def,
+		0xff75cdff,
+		0xff70f0a7,
+		0xff64b738,
+		0xff797125,
+		0xff6f3629,
+		0xffc95d3b,
+		0xfff6a641,
+		0xfff7ef73,
+		0xfff4f4f4,
+		0xffc2b094,
+		0xff866c56,
+		0xff573c33,
+};
